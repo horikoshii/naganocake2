b5b21056e7095fff637f4653455a02903e3613ff
horikoshii <achiga9696@gmail.com>
2023-01-08 12:15:30 +0000

    genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,20 +1,26 @@
 class Admin::GenresController < ApplicationController
-  
+
   def index
     @genre = Genre.new
     @genres = Genre.all
   end
-  
+
   def edit
     @genre = Genre.find(params[:id])
   end
-  
+
   def create
-    @gebre = Genre.new
+    @genre = Genre.new(genre_params)
     @genre.save
     redirect_to admin_genres_path
   end
-  
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
+  end
+
   private
   def genre_params
     params.require(:genre).permit(:name)
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,19 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def show
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
   end
 end
