4e377a520dadbfa7b5f945da6d76e19535f59a26
horikoshii <achiga9696@gmail.com>
2023-01-13 10:38:57 +0000

    item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,11 +10,23 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
   end
 
   def create
     @item = Item.new(item_params)
     @item.save
+    redirect_to admin_item_path(@item.id)
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params
+    )
     redirect_to admin_item_path(@item.id)
   end
 
