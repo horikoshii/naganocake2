7aa13b1b05b44b919b822f24934e922873033f66
horikoshii <achiga9696@gmail.com>
2023-01-19 10:48:14 +0000

    about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: "homes#top"
+  get "/about",to: "homes#about"
   get "/admin", to: "admin/homes#top"
 
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
