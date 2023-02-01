1baf3b466153dfbdb39b24ee21f99af2c33191c5
horikoshii <achiga9696@gmail.com>
2023-02-01 10:26:29 +0000

    items

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,22 @@
 class Public::ItemsController < ApplicationController
 
   def index
+    @genres=Genre.all
+    @items=Item.all
+  end
+
+  def serch
+    @genres=Genre.all
+    @items=Item.all
+    if params[:genre_id]
+      @genre=Genre.find(params[:genre_id])
+      @items=@genre.items
+    end
+  end
+
+  def show
+    @genres=Genre.all
+    @item=Item.find(params[:id])
+    @cart_item=CartItem.new
   end
 end
