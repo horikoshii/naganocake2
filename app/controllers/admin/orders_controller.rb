009ff61d810fc3770fe68456ba69a442c022b73e
horikoshii <achiga9696@gmail.com>
2023-02-18 14:08:44 +0000

    admin_order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,9 @@
 class Admin::OrdersController < ApplicationController
-  
+
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
+    @subtotal = @order.order_details.inject(0) { |sum, order_detail| sum + order_detail.subtotal }
+    @total = @order.order_details.inject(0) { |sum, order_detail| sum + order_detail.subtotal } + 800
   end
 end
